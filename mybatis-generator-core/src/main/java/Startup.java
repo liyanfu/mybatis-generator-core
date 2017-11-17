658c5bb1e2528af15d4a1c7c579d0a2c11d89212
fury <250977428@qq.com>
2017-11-17 15:16:07 +0800

    新增xml,java生成的工具类 新增批量插入方法插件

--- mybatis-generator-core/src/main/java/Startup.java
+++ mybatis-generator-core/src/main/java/Startup.java
@@ -1,30 +1,21 @@
 
-
-
 import org.mybatis.generator.api.ShellRunner;
 
 /**
  * @author fury
- *      
+ * @date : 2017年11月17日 下午2:37:33
  */
 public class Startup {
 
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception {
+		// 调试初始化参数
+		Startup startup = new Startup();
+		// 取得根目录路径
+		String rootPath = startup.getClass().getResource("/").getFile().toString();
+		// 获取配置文件
+		String[] arg = new String[] { "-configfile", rootPath + "conf/generatorConfig.xml", "-overwrite" };
+		ShellRunner.main(arg);
 
-        // 调试初始化参数
-        Startup startup = new Startup();
-        //取得根目录路径
-        String rootPath = startup.getClass().getResource("/").getFile().toString();
-        //当前目录路径
-     // String rootPath=startup.getClass().getResource(".").getPath();
-     // String rootPath=startup.getClass().getResource("").getFile().toString();
-        //当前目录的上级目录路径
-    		  // String rootPath=startup.getClass().getResource("../").getFile().toString();
-        String[] arg = new String[]{"-configfile", rootPath + "conf/generatorConfig.xml", "-overwrite"};
-        //String[] arg = new String[]{"-configfile", rootPath + "test/generatorConfigForMySql.xml", "-overwrite"};
-
-        ShellRunner.main(arg);
-
-    }
+	}
 }
